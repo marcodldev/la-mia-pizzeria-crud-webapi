3fc073409acedbe2108c3bef049f1cb2558ffc58
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-04-18 16:21:19 +0200

    + get (id), put, delete

--- la-mia-pizzeria-static/Controllers/PizzaApiController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaApiController.cs
@@ -1,6 +1,7 @@
 ﻿using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+//using System.Data.Entity;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -9,15 +10,73 @@ namespace la_mia_pizzeria_static.Controllers
     public class PizzaApiController : ControllerBase
     {
 
-        [HttpGet]
-        public IActionResult GetPizzas()
-        {
-            using (PizzaContext ctx = new PizzaContext())
-            {
-                IQueryable<Pizza> pizzas = ctx.Pizze;
+        private readonly PizzaContext _context;
 
-                return Ok(pizzas.ToList());
+        public PizzaApiController(PizzaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPizzas([FromQuery] string? name)
+        {
+            var pizzas = _context.Pizze
+                //.Include(p => p.Categorie)
+                //.Include(p => p.Ingredienti)
+                .Where(p => name == null || p.Name.ToLower().Contains(name.ToLower())).ToList();
+
+            return Ok(pizzas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPizza(int id)
+        {
+            var pizza = _context.Pizze.FirstOrDefault(p => p.Id == id);
+
+            if (pizza is null)
+            {
+                return NotFound();
             }
+
+            return Ok(pizza);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutPizza(int id, [FromBody] Pizza pizza)
+        {
+            var pizzaSalvata = _context.Pizze.FirstOrDefault(p => p.Id == id);
+
+            if (pizzaSalvata is null)
+            {
+                return NotFound();
+            }
+
+            pizzaSalvata.Name = pizza.Name;
+            pizzaSalvata.ImgUrl = pizza.ImgUrl;
+            pizzaSalvata.Description = pizza.Description;
+            pizzaSalvata.Prezzo = pizza.Prezzo;
+            pizzaSalvata.CategorieId = pizza.CategorieId;
+
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePizza(int id)
+        {
+            var pizzaSalvata = _context.Pizze.FirstOrDefault(p => p.Id == id);
+
+            if (pizzaSalvata is null)
+            {
+                return NotFound();
+            }
+
+            _context.Pizze.Remove(pizzaSalvata);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
     }
