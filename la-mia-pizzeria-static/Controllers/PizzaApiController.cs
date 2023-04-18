97c40340937fe40d30b977dcdd0a8ae9215eab1e
Emir-Software <125670961+Emir-Software@users.noreply.github.com>
2023-04-18 14:34:21 +0200

    + metodo getpizzas (get)

---
+++ la-mia-pizzeria-static/Controllers/PizzaApiController.cs
@@ -0,0 +1,24 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaApiController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetPizzas()
+        {
+            using (PizzaContext ctx = new PizzaContext())
+            {
+                IQueryable<Pizza> pizzas = ctx.Pizze;
+
+                return Ok(pizzas.ToList());
+            }
+        }
+
+    }
+}
